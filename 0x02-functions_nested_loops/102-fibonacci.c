b2ea81334dbcf5a29be54ebbd7c8671f407028da
Nousairasyl <nousair.asyl@gmail.com>
2023-02-21 19:15:21 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints the sum of all the multiples of 3 or 5 below 1024
+ * main - Prints the sum of all the multiples of 3 or 5 below 1024.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int i, sum = 0;
+    unsigned long fib1 = 0, fib2 = 1, fib_sum = 0, even_sum = 0;
 
-    for (i = 0; i < 1024; i++)
+    while (fib_sum < 4000000)
     {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
+        fib_sum = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = fib_sum;
+        if (fib_sum % 2 == 0)
+            even_sum += fib_sum;
     }
 
-    printf("%d\n", sum);
+    printf("%lu\n", even_sum);
 
-    return 0;
+    return (0);
 }
