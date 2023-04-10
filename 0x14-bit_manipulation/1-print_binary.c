f4fe8e55063023505e509263f3484f45bfed8ec1
Nousairasyl <nousair.asyl@gmail.com>
2023-04-09 17:55:37 -0700

    1-prin

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,27 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned long int mask = 1UL << 63; /* start with most significant bit */
+	int i, j;
 
-	/* find most significant bit */
-	for (i = 0; i < 64; i++)
+	if (n == 0)
 	{
-		if ((n & mask) != 0)
-			break;
-		mask >>= 1; /* shift mask right */
+		_putchar('0');
+		return;
 	}
 
-	/* print each bit */
-	for (; i < 64; i++)
+	/* Find the most significant bit */
+	for (i = 63; i >= 0; i--)
 	{
-		_putchar((n & mask) != 0 ? '1' : '0');
-		mask >>= 1; /* shift mask right */
+		if ((n >> i) & 1)
+			break;
+	}
+
+	/* Print each bit */
+	for (j = i; j >= 0; j--)
+	{
+		if ((n >> j) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
 	}
 }
