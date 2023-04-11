c4575781400834fd87f3d62765cb10458ff98737
Nousairasyl <nousair.asyl@gmail.com>
2023-04-10 18:51:47 -0700

    trxt

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,21 @@
-#pragma once
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <stddef.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-ssize_t read_textfile(const char* filename, size_t letters);
-int create_file(const char* filename, const char* text_content);
-int append_text_to_file(const char* filename, const char* text_content);
+/* Function that calculates the factorial of a number */
+int factorial(int n);
+
+/* Function that calculates the nth fibonacci number */
+int fibonacci(int n);
+
+/* Function that calculates the area of a circle given its radius */
+double area_of_circle(double radius);
+
+#endif
