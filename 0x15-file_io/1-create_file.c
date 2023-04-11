a3641660dad768a27fb854bf2ac1b0627d9b6726
Nousairasyl <nousair.asyl@gmail.com>
2023-04-10 19:52:03 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return (-1);
+	int o, w, len = 0;
 
-    FILE *fp = fopen(filename, "w");
-    if (fp == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL) {
-        fprintf(fp, "%s", text_content);
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-    fclose(fp);
-    return (1);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
