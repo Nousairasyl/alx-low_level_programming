38668a6cde6c01f13c3e093d8e595d4a0ef4752b
Nousairasyl <nousair.asyl@gmail.com>
2023-04-10 18:54:04 -0700

    text

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,15 +7,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
+#include <elf.h>
 
-/* Function that calculates the factorial of a number */
-int factorial(int n);
-
-/* Function that calculates the nth fibonacci number */
-int fibonacci(int n);
-
-/* Function that calculates the area of a circle given its radius */
-double area_of_circle(double radius);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
